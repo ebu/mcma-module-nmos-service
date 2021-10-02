9e863f7fd0964ac5986521d0a42378ed214232a4
Joost Rovers <joost@rovers.pt>
2021-10-02 15:56:33 +0100

    Added post deployment script

---
+++ scripts/post-deploy/src/index.ts
@@ -0,0 +1,24 @@
+import * as fs from "fs";
+import * as AWS from "aws-sdk";
+import { exportVpnClientConfig } from "./export-vpn-client-config";
+
+const AWS_CREDENTIALS = "../../deployment/aws-credentials.json";
+const TERRAFORM_OUTPUT = "../../deployment/terraform.output.json";
+
+AWS.config.loadFromPath(AWS_CREDENTIALS);
+
+async function main() {
+    try {
+        const terraformOutput = JSON.parse(fs.readFileSync(TERRAFORM_OUTPUT, "utf8"));
+
+        await exportVpnClientConfig(terraformOutput, AWS);
+    } catch (error) {
+        if (error.response && error.response.data) {
+            console.error(JSON.stringify(error.response.data, null, 2));
+        } else {
+            console.error(error);
+        }
+    }
+}
+
+main().then(() => console.log("Done"));
